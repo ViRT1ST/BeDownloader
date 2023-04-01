0f4d496d2b9279834fa9453dc508979987e0a8c6
unknown <ViRT1ST@outlook.com>
2023-04-01 15:46:46 +0600

    First working version

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
 const puppeteer = require('puppeteer');
 
 const {
-  autoScroll,
+  addUserConfigToAppConfig,
   disableImagesLoading,
-  getMoodboardLinks,
-  getDataFromProject,
-  updateProjectData,
+  generateProjectsList,
+  downloadProjects,
 } = require('./services');
-
-const userConfig = {
-  dynamicLoadingTimeout: 10000,
-  urls: [
-    'https://www.behance.net/collection/202865305/TEST',
-    'https://www.behance.net/gallery/166160265/Koala-House'
-  ],
-};
 
 const mode = 'development';
 
@@ -31,72 +22,35 @@ const browserOptions = {
   }
 };
 
+const userConfig = {
+  inMoodboardTimeout: 10000,
+  betweenProjectsDelay: 1000,
+  betweenDownloadsDelay: 1000,
+  page: null,
+  outputDir: 'E:/Downloads',
+  userUrls: [
+    // 'https://www.behance.net/collection/202865305/TEST',
+    // 'https://www.behance.net/gallery/166160265/Koala-House'
+    'https://www.behance.net/gallery/155176025/Private-Offices'
+  ],
+};
 
 async function runBrowser() {
-  const { urls, dynamicLoadingTimeout } = userConfig;
-
   const browser = await puppeteer.launch(browserOptions[mode]);
   const page = await browser.newPage();
 
-  await disableImagesLoading(page);
+  userConfig.page = page;
+  addUserConfigToAppConfig(userConfig);
 
-  async function parseMoodboardProjects(url) {
-    await page.goto(url);
-    await page.waitForSelector('.Collection-wrapper-LHa');
+  await disableImagesLoading();
 
-    console.log('Scrolling moodboard to page end: begin ...');
-    await autoScroll(page, dynamicLoadingTimeout);
-    console.log('Scrolling moodboard to page end: ended.');
+  await generateProjectsList();
 
-    console.log('Parsing links from moodboard: begin ...');
-    const moodboardProjects = await getMoodboardLinks(page);
-    console.log('Parsing links from moodboard: ended.');
-
-    return moodboardProjects;
-  }
-
-  async function generateProjectsList() {
-    let projects = [];
-
-    for (const url of urls) {
-      if (url.includes('behance.net/collection/')) {
-        const moodboardProjects = await parseMoodboardProjects(url);
-        projects = [...projects, ...moodboardProjects];
-      } else {
-        projects.push(url);
-      }
-    }
-
-    return projects;
-  }
-
-  async function parseProjectData(url) {
-    const data = await getDataFromProject(page, url);
-    const updatedData = await updateProjectData(data);
-    return updatedData;
-  }
-
-  async function parseDataFromAllProjects(urls) {
-    const allData = [];
-
-    for (const url of urls) {
-      const projectData = await parseProjectData(url);
-      allData.push(projectData);
-    }
-
-    return allData;
-  }
-
-  const projectsUrls = await generateProjectsList();
-  console.log(projectsUrls);
-  const allData = await parseDataFromAllProjects(projectsUrls);
-  console.log(allData);
+  await downloadProjects();
 
 
-  setTimeout(() => {
-    browser.close();
-  }, 1000);
 
+  browser.close();
 }
 
 
--- src/services.js
+++ src/services.js
@@ -1,7 +1,26 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
 const { transliterate } = require('transliteration');
 
-async function disableImagesLoading(page) {
+let appConfig = {
+  inMoodboardTimeout: 10000,
+  betweenProjectsDelay: 1000,
+  betweenDownloadsDelay: 1000,
+  page: null,
+  outputDir: null,
+  userUrls: [],
+  projects: []
+};
+
+function addUserConfigToAppConfig(userConfig) {
+  appConfig = { ...appConfig, ...userConfig };
+}
+
+async function disableImagesLoading() {
+  const { page } = appConfig;
+
   await page.setRequestInterception(true);
+
   page.on('request', (interceptedRequest) => {
     if (interceptedRequest.resourceType() === 'image') {
       interceptedRequest.abort();
@@ -11,77 +30,22 @@ async function disableImagesLoading(page) {
   });
 }
 
-async function enableImagesLoading(page) {
+async function enableImagesLoading() {
+  const { page } = appConfig;
+
   await page.setRequestInterception(false);
 }
 
-async function getMoodboardLinks(page) {
-  return page.evaluate(() => {
-    const selector = '.js-project-cover-title-link';
-    const items = Array.from(document.querySelectorAll(selector));
-    const urls = items.map((item) => item.getAttribute('href'));
-    return urls;
-  });
-}
+async function autoScroll() {
+  const { inMoodboardTimeout, page } = appConfig;
 
-
-
-async function getDataFromProject(page, url) {
-  await page.goto(url);
-  await page.waitForSelector('.project-content-wrap');
-
-  return page.evaluate(() => {
-    function getMetaProperty(propertyName) {
-      return document.head.querySelector(`meta[property="${propertyName}"]`)
-        .getAttribute('content');
-    }
-
-    function getProjectmages() {
-      const replacements = [
-        'project_modules/2800/',
-        'project_modules/2800_opt_1/',
-        'project_modules/1400/',
-        'project_modules/1400_opt_1/',
-        'project_modules/disp/',
-        'project_modules/max_1200/',
-        'project_modules/fs/'
-      ];
-
-      const elements = Array.from(document.querySelectorAll('img'));
-      const urls = elements.map((item) => item.getAttribute('src'));
-
-      const urlsLQ = urls.filter((item) => item.includes('project_modules'));
-      const urlsHQ = urlsLQ.map((url) => {
-        for (const replacement of replacements) {
-          url = url.replace(replacement, 'project_modules/source/');
-        }
-        return url;
-      });
-
-      return urlsHQ;
-    }
-
-    const title = getMetaProperty('og:title');
-    const owners = getMetaProperty('og:owners');
-    const projectImages = getProjectmages();
-    const url = getMetaProperty('og:url');
-    const id = url.split('/')[4];
-
-    console.log('projectImages: ', projectImages);
-
-    return { title, owners, url, id, projectImages };
-  });
-}
-
-
-async function autoScroll(page, dynamicLoadingTimeout) {
-  await page.evaluate(async (dynamicLoadingTimeout) => {
+  await page.evaluate(async (inMoodboardTimeout) => {
     await new Promise((resolve) => {
       const scrollStepTime = 500;
       const scrollStepHeight = 500;
 
       const scrollCheckTime = 1000;
-      const maxScrollMatches = dynamicLoadingTimeout / scrollCheckTime;
+      const maxScrollMatches = inMoodboardTimeout / scrollCheckTime;
       let currentScrollMatches = 0;
       let lastScrollPosition = 0;
 
@@ -102,9 +66,48 @@ async function autoScroll(page, dynamicLoadingTimeout) {
         }
       }, scrollCheckTime);
     });
-  }, dynamicLoadingTimeout);
+  }, inMoodboardTimeout);
 }
 
+async function getMoodboardLinks(url) {
+  const { page } = appConfig;
+
+  await page.goto(url);
+  await page.waitForSelector('.Collection-wrapper-LHa');
+
+  console.log('Scrolling moodboard to page end: begin ...');
+  await autoScroll(page);
+  console.log('Scrolling moodboard to page end: ended.');
+
+  return page.evaluate(() => {
+    console.log('Parsing links from moodboard: begin ...');
+
+    const selector = '.js-project-cover-title-link';
+    const items = Array.from(document.querySelectorAll(selector));
+    const urls = items.map((item) => item.getAttribute('href'));
+
+    console.log('Parsing links from moodboard: ended.');
+    return urls;
+  });
+}
+
+async function generateProjectsList() {
+  const { userUrls } = appConfig;
+
+  let projects = [];
+
+  for (const url of userUrls) {
+    if (url.includes('behance.net/collection/')) {
+      const moodboardProjects = await getMoodboardLinks(url);
+      projects = [...projects, ...moodboardProjects];
+    } else {
+      projects.push(url);
+    }
+  }
+
+  appConfig.projects = projects;
+  console.log(appConfig.projects);
+}
 
 function transliterateString(string) {
   return transliterate(string);
@@ -119,18 +122,116 @@ function convertToKebabCase(string) {
     .toLowerCase().replace(/--/g, '-');
 }
 
-function updateProjectData(object) {
-  const updatedData = JSON.parse(JSON.stringify(object));
-  const { title, owners } = updatedData;
-  updatedData.normalizedTitle = convertToKebabCase(title);
-  updatedData.normalizedOwners = convertToKebabCase(owners);
-  return updatedData;
+async function parseProjectData(url) {
+  const { page } = appConfig;
+
+  await page.goto(url);
+  await page.waitForSelector('.project-content-wrap');
+
+  return page.evaluate(() => {
+    function getMetaProperty(propertyName) {
+      return document.head.querySelector(`meta[property="${propertyName}"]`)
+        .getAttribute('content');
+    }
+
+    const title = getMetaProperty('og:title');
+    const owners = getMetaProperty('og:owners');
+    const url = getMetaProperty('og:url');
+    const id = url.split('/')[4];
+
+    const imgList = Array.from(document.querySelectorAll('img'));
+    const srcList = imgList.map((item) => item.getAttribute('src'));
+    const projectImages = srcList.filter((item) => item.includes('project_modules'));
+
+    return { title, owners, url, id, projectImages };
+  });
 }
 
+function correctProjectData(data) {
+  const { title, owners, url, id, projectImages } = data;
 
+  const normalizedTitle = convertToKebabCase(title);
+  const normalizedOwners = convertToKebabCase(owners);
+
+  const replacements = [
+    'project_modules/2800/',
+    'project_modules/2800_opt_1/',
+    'project_modules/1400/',
+    'project_modules/1400_opt_1/',
+    'project_modules/disp/',
+    'project_modules/max_1200/',
+    'project_modules/fs/'
+  ];
+
+  const images = projectImages.map((url) => {
+    for (const replacement of replacements) {
+      url = url.replace(replacement, 'project_modules/source/');
+    }
+    return url;
+  });
+
+  return { id, url, title, owners, normalizedTitle, normalizedOwners, images };
+}
+
+async function getProjectData(url) {
+  const data = await parseProjectData(url);
+  const correctedData = await correctProjectData(data);
+  return correctedData;
+}
+
+async function downloadFile(url, path) {
+  const res = await fetch(url);
+  const fileStream = fs.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on('error', () => {
+      reject();
+    });
+    fileStream.on('finish', () => {
+      resolve();
+    });
+  });
+}
+
+function addZeroForNumberLessTen(number) {
+  console.log(number);
+  return number < 10 ? `0${number}` : number.toString();
+}
+
+async function downloadProjects() {
+  const {
+    betweenProjectsDelay,
+    betweenDownloadsDelay,
+    page,
+    outputDir,
+    projects
+  } = appConfig;
+
+  for (const project of projects) {
+    const projectData = await getProjectData(project);
+    const { id, normalizedTitle, normalizedOwners, images } = projectData;
+
+    let counter = 1;
+    for (const item of images) {
+      const prefix = 'behance_';
+      const number = addZeroForNumberLessTen(counter);
+      const extension = item.split('.').pop();
+      const template = `${normalizedOwners}_${id}-${normalizedTitle}`;
+      const path = `${outputDir}/${prefix}${template}_${number}.${extension}`;
+
+      await downloadFile(item, path);
+      counter += 1;
+      console.log(path);
+
+      await page.waitForTimeout(betweenDownloadsDelay);
+    }
+
+    await page.waitForTimeout(betweenProjectsDelay);
+  }
+}
+
+module.exports.addUserConfigToAppConfig = addUserConfigToAppConfig;
 module.exports.disableImagesLoading = disableImagesLoading;
 module.exports.enableImagesLoading = enableImagesLoading;
-module.exports.getMoodboardLinks = getMoodboardLinks;
-module.exports.getDataFromProject = getDataFromProject;
-module.exports.updateProjectData = updateProjectData;
-module.exports.autoScroll = autoScroll;
+module.exports.generateProjectsList = generateProjectsList;
+module.exports.downloadProjects = downloadProjects;
